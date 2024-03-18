9c8ba043a33206087ca1ba52ed77f98e5515d8eb
Aline Rodrigues Pereira <110029532+alinerp90@users.noreply.github.com>
2024-03-18 11:23:31 +0000

    removed useless comments

--- main.py
+++ main.py
@@ -23,16 +23,6 @@ def write_state(answer_state):
     pen.write(answer_state)
 
 
-coord = data[data.state == "Hawaii"]
-print(coord)
-x = list(coord.x)[0]
-print(x)
-y = list(coord.y)[0]
-print(y)
-
-# write_state("Ohio")
-
-
 end_game = False
 score = 0
 list_guess = []
@@ -54,19 +44,3 @@ while end_game is False:
 
 screen.exitonclick()
 
-"""
-1.Convert the guess to title case = ok
-2. Check if the guess is among the 50 states = ok
-3. Write correct guesses onto map
-4.Use a loop to allow the user to keep guessing = ok
-5. Record the correct guesses in a list = ok
-6. Keep track oh the score
-"""
-
-
-# Code to get the x,y coord when clicking on the screen
-# def get_mouse_click_coor(x, y):
-#     print(x, y)
-
-# turtle.onscreenclick(get_mouse_click_coor)
-# turtle.mainloop() #maintain the screen opened 
